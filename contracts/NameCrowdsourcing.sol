b8f052b27e4ad3df37fdfa9317bb7880cd2cbe7e
Peter_Ujvari <Peter_Ujvari@epam.com>
2017-05-25 18:03:23 +0200

    constructor

--- contracts/NameCrowdsourcing.sol
+++ contracts/NameCrowdsourcing.sol
@@ -8,5 +8,17 @@ contract NameCrowdsourcing {
     address beneficiary;
     address[] funderList;
     mapping(address => uint) fundings;
-    
+    AbstractENS deployedENS;
+    AbstractRegistrar deployedRegistrar;
+
+    function NameCrowdsourcing(string _ensName, address _beneficiary, address ensAddress, address registrarAddress){
+        ensName = _ensName;
+        beneficiary = _beneficiary;
+        deployedENS = AbstractENS(ensAddress);
+        deployedRegistrar = AbstractRegistrar(registrarAddress);
+    }
+
+    function () payable {
+        
+    }
 }
